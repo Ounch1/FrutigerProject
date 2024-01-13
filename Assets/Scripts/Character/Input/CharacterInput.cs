0503635e31146c10b2138bddeda2f3dfe7419f5d
Mitrofan <118289864+ZloyKorovanovich@users.noreply.github.com>
2024-01-13 20:23:04 +0300

    Add files via upload

---
+++ Assets/Scripts/Character/Input/CharacterInput.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class CharacterInput : MonoBehaviour, IMoverInput
+{
+    public Action onSetInputs;
+
+    private bool _isShift;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            _isShift = !_isShift;
+    }
+
+    public void GetMovementAxis(out float vertical, out float horizontal, out bool isShift)
+    {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        isShift = _isShift;
+    }
+
+    public void SubscribeOnInput(Action action)
+    {
+        onSetInputs += action;
+    }
+
+    public void UnsubscribeOnInput(Action action)
+    {
+        onSetInputs -= action;
+    }
+}
